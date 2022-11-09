93f4104bac6129a30ced7ca2d420763ac532ab72
Youssef-zr <yn-neinaa@hotmail.com>
2022-11-09 19:37:39 +0100

    09-11-2022

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,21 +26,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (Schema::hasTable('colis')) {
-            if (!Schema::hasColumn('colis', 'signature') and !Schema::hasColumn('colis', 'signature')) {
-                Schema::table('colis', function (Blueprint $table) {
-                    DB::statement("ALTER TABLE colis ADD signature longblob");
-                    DB::statement("ALTER TABLE colis ADD recu longblob");
-                });
-            }
-        }
+        // if (Schema::hasTable('colis')) {
+        //     if (!Schema::hasColumn('colis', 'signature') and !Schema::hasColumn('colis', 'signature')) {
+        //         Schema::table('colis', function (Blueprint $table) {
+        //             DB::statement("ALTER TABLE colis ADD signature longblob");
+        //             DB::statement("ALTER TABLE colis ADD recu longblob");
+        //         });
+        //     }
+        // }
 
-        if (Schema::hasTable('paiment')) {
-            if (!Schema::hasColumn('paiment', 'recu_paiement')) {
-                Schema::table('colis', function (Blueprint $table) {
-                    DB::statement("ALTER TABLE paiment ADD signature longblob");
-                });
-            }
-        }
+        // if (Schema::hasTable('paiment')) {
+        //     if (!Schema::hasColumn('paiment', 'recu_paiement')) {
+        //         Schema::table('colis', function (Blueprint $table) {
+        //             DB::statement("ALTER TABLE paiment ADD signature longblob");
+        //         });
+        //     }
+        // }
     }
 }
